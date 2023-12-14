f84ffe2b21a4a02199bb32db274361f2ca41851c
Andrea Dayo <andrealouisedayo@gmail.com>
2023-12-14 20:30:14 +0800

    styles added

--- db/schema.rb
+++ db/schema.rb
@@ -13,7 +13,7 @@
 ActiveRecord::Schema[7.1].define(version: 2023_12_14_044217) do
   create_table "posts", force: :cascade do |t|
     t.string "title"
-    t.text "body"
+    t.text :body
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end

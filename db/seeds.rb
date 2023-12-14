8087c2da847ee5efc89d7c75351a46be0fcc23b6
Andrea Dayo <andrealouisedayo@gmail.com>
2023-12-14 12:47:09 +0800

    simple posts

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,12 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+# Clear existing records to avoid duplication when re-running the seed file
+Post.destroy_all
+
+# Create sample posts
+Post.create(title: 'First Post', body: 'This is the content of the first post.')
+Post.create(title: 'Second Post', body: 'Another exciting post for the blog.')
+Post.create(title: 'Third Post', body: 'The third post is here. Enjoy reading!')
+
+# You can also create more posts as needed
+# Post.create(title: 'Fourth Post', body: '...')
+
+puts 'Seed data created successfully!'
